443c4c81346a62f16728471e13fd4d06d3c0a621
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2019-01-27 13:54:01 +0100

    Set .gitignore_global as the exlude file in install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ all_dotfiles=".vimrc .tmux.conf .gitignore_global"
 for dotfile in $all_dotfiles; do
     ln -f -s $PWD/$dotfile ~/$dotfile
 done
+
+git config --global core.excludesfile ~/.gitignore_global
