7f24fa4a95a203a684a952f067bb48dcfdd43bae
Filip Binkiewicz <filip.binkiewicz@braincorp.com>
2020-10-14 15:29:54 -0700

    Install a few thing

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 all_dotfiles=".vimrc .tmux.conf .gitignore_global"
 
+echo "Installing plug.vim"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing tpm"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
 for dotfile in $all_dotfiles; do
     ln -f -s $PWD/$dotfile ~/$dotfile
 done
