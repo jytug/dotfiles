4b00e00c7174b2f25c63d380522c38d2922e0d04
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2023-01-01 23:42:39 +0000

    Minor refactor

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
-all_dotfiles=$(ls .*)
+all_dotfiles=".vimrc .tmux.conf .gitignore_global"
 
 for dotfile in $all_dotfiles; do
-    cp -r $PWD/$dotfile ~/$dotfile
+    cp $PWD/$dotfile ~/$dotfile
 done
 
+echo "Installing VimPlug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "...done"
+
+echo "Installing tpm"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "...done"
+
+echo "Setting up global gitignore..."
 git config --global core.excludesfile ~/.gitignore_global
+echo "...done"
